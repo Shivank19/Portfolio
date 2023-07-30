5e7dffc2ca403dfab2ed75e50b7cfecdbf26c247
Shivank19 <shivank.sapra@gmail.com>
2023-07-31 01:04:43 +0530

    schema setup

--- backend-sanity/portfolio1/schemas/index.js
+++ backend-sanity/portfolio1/schemas/index.js
@@ -1,1 +1,19 @@
-export const schemaTypes = []
+import testimonials from './testimonials'
+import abouts from './abouts'
+import contact from './contact'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import works from './works'
+import brands from './brands'
+
+export const schemaTypes = [
+  works,
+  testimonials,
+  brands,
+  abouts,
+  skills,
+  workExperience,
+  experiences,
+  contact,
+]
